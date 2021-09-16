4ceb200d4c349eea9ce9126d4cfdeaf8776ea799
Jenny Ngo <myduyen27397@gmail.com>
2021-09-16 19:33:14 +1000

    finish elearning website

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-
-
+/* Truncate Card Title */
 window.addEventListener("load",function(){
     truncateCardTitle();
 })
@@ -20,3 +19,17 @@ function truncateString(str,num){
         return str;
     }
 }
+
+/* Sidebar Mini */
+var toggleBtn = document.querySelector('.sidebarMini__button');
+var sibebarMini = document.querySelector('.sidebarMini');
+var switchBtn = document.querySelector('#checkbox');
+
+toggleBtn.addEventListener('click', function(){
+    sibebarMini.classList.toggle('is-opened'); 
+});
+switchBtn.addEventListener('click', function(){
+    document.querySelector('body').classList.toggle('darkMode');
+    
+});
+
